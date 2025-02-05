de1923f6235f17d24c03743044fee2b9576c489e
nooralam867 <72295326+nooralam867@users.noreply.github.com>
2025-02-05 23:40:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let imagebtn=document.querySelector("#image")
 let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
-const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=Your-Api-Key"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDLEx9bvCCmyhRuTCl4VijpJMTPTz5UlpA"
 
 let user={
     message:null,
